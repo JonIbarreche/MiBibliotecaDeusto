02319407bf91afe947237c48efd8a2cc03bd2fdc
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2021-03-29 14:49:29 +0200

    Libro.h prueba
    Primer commit

---
+++ Libro.h
@@ -0,0 +1,31 @@
+#ifndef LIBRO_H_
+#define LIBRO_H_
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Libro
+{
+public: 
+    int nSerie;
+    string autor;
+    int fechaLanzamiento;
+
+public:
+    Libro(const int nSerie, const string autor, const int fechaLanzamiento);
+    Libro();
+
+    Libro(Libro &j);
+
+    void imprimir();
+    int getNSerie() const;
+    string getAutor() const;
+    int getFechaLanzamiento() const;
+
+    void setNSerie(int nSerie);
+    void setAutor(const string &autor);
+    void setFechaLanzamiento(int fechaLanzamiento);
+};
+#endif
