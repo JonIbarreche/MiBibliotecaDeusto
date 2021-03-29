28f986d19c533e316339271f086f2dd2ecf4240e
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2021-03-29 17:06:03 +0200

    Update Libro.h

--- Libro.h
+++ Libro.h
@@ -1,12 +1,13 @@
 #ifndef LIBRO_H_
 #define LIBRO_H_
 
-#include <iostream>
-#include <string.h>
+//#include <iostream>
+//#include <string.h>
 
-typedef struct {
-	int nSerie;
-	char autor;
+typedef struct
+{
+    int nSerie;
+    char autor;
     int fechaLanzamiento;
 } Libro;
 
