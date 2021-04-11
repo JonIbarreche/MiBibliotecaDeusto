34c141f12c4e1ae26e48f38940641d3205f14fcf
xabie <xabie@192.168.1.34>
2021-04-11 18:00:08 +0200

    Menu (falta que se pare para meter opciones)

--- Main.c
+++ Main.c
@@ -1,11 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "Libro.h"
 #include "Cliente.h"
 #include "Biblioteca.h"
 
 int main(void){
 
-    
-    return 0;
+			int opcion;
+			int usuario;
+			int contrasena;
+			bool bucle = true;
+			while (bucle){
+			printf("Menu Principal\n\n");
+			printf( "\n   Escoge una opción:" );
+	 	 	printf( "\n   1. Iniciar sesión como usuario." );
+	        printf( "\n   2. Iniciar sesión como trabajador." );
+	        printf( "\n   3. Salir.");
+
+	        scanf( "%d", &opcion );
+
+	        switch ( opcion )
+	                {
+	                    case 1: printf( "\n   Introduzca su usuario: ");
+	                            scanf( "%d", &usuario );
+	                            printf( "\n   Introduzca su contraseña: ");
+	                            scanf( "%d", &contrasena );
+	                            printf( "\n   Inicio de sesión con exito! ");
+	                            printf( "\n");
+	                            printf( "\n   Seleccione acción: ");
+	                            printf( "\n   1. Reservar libro. ");
+	                            printf( "\n   2. Devolver libro. ");
+	                            scanf( "%d", &opcion );
+
+	                            switch (opcion){
+
+	                            		case 1: printf("\n Libro reservado");
+	                            				break;
+	                            		case 2: printf("\n Libro devuelto");
+	                            				break;
+	                            }
+
+
+	                            break;
+
+	                    case 2: printf( "\n   Introduzca su usuario: ");
+	                    		scanf( "%d", &usuario );
+                        		printf( "\n   Introduzca su contraseña: ");
+                        		scanf( "%d", &contrasena );
+                        		printf( "\n   Inicio de sesión con exito! ");
+                        		printf( "\n");
+                        		printf( "\n   Seleccione acción: ");
+                        		printf( "\n   1. Reservar libro. ");
+                        		printf( "\n   2. Devolver libro. ");
+                        		printf( "\n   3. Añadir libro. ");
+                        		printf( "\n   4. Eliminar libro. ");
+                        		scanf( "%d", &opcion );
+
+                        switch (opcion){
+
+                        		case 1: printf("\n Libro reservado.");
+                        				break;
+                        		case 2: printf("\n Libro devuelto.");
+                        				break;
+                        		case 3: printf("\n Libro añadido.");
+                        		        break;
+                        		case 4: printf("\n Libro eliminado.");
+                        		        break;
+                        }
+
+
+                        break;
+
+	                    case 3: printf( "\n   Adiós. ");
+
+	                    break;
+
+	                    default:
+	                    printf("Opcion no valida vuelva a intentar");
+
+	                 }
+			}
+
 }
