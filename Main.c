a6b8348b5b14679fb22d64a4f4af2444a9410439
leiregago <leire.gago@opendeusto.es>
2021-04-12 09:56:19 +0200

    arreglo main

--- Main.c
+++ Main.c
@@ -12,82 +12,92 @@ int main(void){
 			int contrasena;
 			bool bucle = true;
 			while (bucle){
-			printf("Menu Principal\n\n");
-			printf( "\n   Escoge una opción:" );
-	 	 	printf( "\n   1. Iniciar sesión como usuario." );
-	        printf( "\n   2. Iniciar sesión como trabajador." );
-	        printf( "\n   3. Registrarse." );
-	        printf( "\n   4. Salir.");
+				printf("Menu Principal\n\n");
+				printf( "\n   Escoge una opción:" );
+				printf( "\n   1. Iniciar sesión como usuario." );
+				printf( "\n   2. Iniciar sesión como trabajador." );
+				printf( "\n   3. Registrarse." );
+				printf( "\n   4. Salir.");
 
-	        scanf( "%d", &opcion );
+				scanf( "%d", &opcion );
 
-	        switch ( opcion )
-	                {
-	                    case 1: printf( "\n   Introduzca su usuario: ");
-	                            scanf( "%d", &usuario );
-	                            printf( "\n   Introduzca su contraseña: ");
-	                            scanf( "%d", &contrasena );
-	                            printf( "\n   Inicio de sesión con exito! ");
-	                            printf( "\n");
-	                            printf( "\n   Seleccione acción: ");
-	                            printf( "\n   1. Reservar libro. ");
-	                            printf( "\n   2. Devolver libro. ");
-	                            scanf( "%d", &opcion );
+				switch ( opcion )
+						{
+							case 1: printf( "\n   Introduzca su usuario: ");
+									fflush(stdin);
+									scanf( "%d", &usuario );
+									printf( "\n   Introduzca su contraseña: ");
+									fflush(stdin);
+									scanf( "%d", &contrasena );
+									printf( "\n   Inicio de sesión con exito! ");
+									printf( "\n");
+									printf( "\n   Seleccione acción: ");
+									printf( "\n   1. Reservar libro. ");
+									printf( "\n   2. Devolver libro. ");
+									fflush(stdin);
+									scanf( "%d", &opcion );
 
-	                            switch (opcion){
+									switch (opcion){
 
-	                            		case 1: printf("\n Libro reservado");
-	                            				break;
-	                            		case 2: printf("\n Libro devuelto");
-	                            				break;
-	                            }
+											case 1: printf("\n Libro reservado");
+													break;
+											case 2: printf("\n Libro devuelto");
+													break;
+									}
 
 
-	                            break;
+									break;
 
-	                    case 2: printf( "\n   Introduzca su usuario: ");
-	                    		scanf( "%d", &usuario );
-                        		printf( "\n   Introduzca su contraseña: ");
-                        		scanf( "%d", &contrasena );
-                        		printf( "\n   Inicio de sesión con exito! ");
-                        		printf( "\n");
-                        		printf( "\n   Seleccione acción: ");
-                        		printf( "\n   1. Reservar libro. ");
-                        		printf( "\n   2. Devolver libro. ");
-                        		printf( "\n   3. Añadir libro. ");
-                        		printf( "\n   4. Eliminar libro. ");
-                        		scanf( "%d", &opcion );
+							case 2: printf( "\n   Introduzca su usuario: ");
+								fflush(stdin);
+									scanf( "%d", &usuario );
+									printf( "\n   Introduzca su contraseña: ");
+										fflush(stdin);
+									scanf( "%d", &contrasena );
+									printf( "\n   Inicio de sesión con exito! ");
+									printf( "\n");
+									printf( "\n   Seleccione acción: ");
+									printf( "\n   1. Reservar libro. ");
+									printf( "\n   2. Devolver libro. ");
+									printf( "\n   3. Añadir libro. ");
+									printf( "\n   4. Eliminar libro. ");
+										fflush(stdin);
+									scanf( "%d", &opcion );
 
-                        switch (opcion){
+							switch (opcion){
 
-                        		case 1: printf("\n Libro reservado.");
-                        				break;
-                        		case 2: printf("\n Libro devuelto.");
-                        				break;
-                        		case 3: printf("\n Libro añadido.");
-                        		        break;
-                        		case 4: printf("\n Libro eliminado.");
-                        		        break;
-                        }
+									case 1: printf("\n Libro reservado.");
+											break;
+									case 2: printf("\n Libro devuelto.");
+											break;
+									case 3: printf("\n Libro añadido.");
+											break;
+									case 4: printf("\n Libro eliminado.");
+											break;
+							}
 
-                        break;
+							break;
 
-	                    case 3: printf( "\n   Introduzca su usuario: ");
-	                    		scanf( "%d", &usuario );
-                        		printf( "\n   Introduzca su contraseña: ");
-                        		scanf( "%d", &contrasena );
-                        		printf( "\n   Registro con exito! ");
-                        		printf( "\n");
-                        break;
+							case 3: printf( "\n   Introduzca su usuario: ");
+								fflush(stdin);
+									scanf( "%d", &usuario );
+									printf( "\n   Introduzca su contraseña: ");
+										fflush(stdin);
+									scanf( "%d", &contrasena );
+									printf( "\n   Registro con exito! ");
+									printf( "\n");
+							break;
 
-	                    case 4: printf( "\n   Adiós. ");
+							case 4: printf( "\n   Adiós. ");
+									bucle = false;
+							break;
 
-	                    break;
+							default:
+							printf("Opcion no valida vuelva a intentar");
 
-	                    default:
-	                    printf("Opcion no valida vuelva a intentar");
-
-	                 }
+						 }
 			}
 
+	return 0;
 }
+
